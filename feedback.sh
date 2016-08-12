a5c91cb5a82c46ccc5f16e0a2e4d3a8e7b68a65e
Paul Harris <paul.harris@eegeo.com>
2016-08-12 16:16:27 +0100

    Show smaller list of errors in makeshow

--- feedback.sh
+++ feedback.sh
@@ -4,6 +4,9 @@ function makeshow()
 {
     make 2> .temp_make_errors
     cat .temp_make_errors
+    echo ""
+    echo "Errors:"
+    grep "error:" .temp_make_errors
     subl `grep "error:" .temp_make_errors | cut -d " " -f1 | sed 's/.$//'`
     rm .temp_make_errors
 }
