9cbae3485a80a3ec62058d922fb12a126801ef70
Vi <1149443+mistodon@users.noreply.github.com>
2016-08-12 14:49:53 +0100

    Add really horrible basic case-insensitivity to ce
    Also handle errors better

--- basics.sh
+++ basics.sh
@@ -1,8 +1,31 @@
 # close enough
 function ce()
 {
+    # Try cd to each argument
+    pushd . > /dev/null
     for arg in "$@"
     do
-        cd *"$arg"*
+        cd *"$arg"* 2>/dev/null
     done
+
+    if [ $? -ne 0 ]; then
+
+        # If it doesn't work, chop off first character of each
+        #   (Really hacky, brittle case-insensitivity)
+        popd > /dev/null
+        pushd . > /dev/null
+        for arg in "$@"
+        do
+            cd *"${arg:1}"* 2>/dev/null
+        done
+
+        if [ $? -ne 0 ]; then
+            echo "ce: '$@': No such file or directory (reached `pwd`)"
+            popd > /dev/null
+        else
+            popd -n > /dev/null
+        fi
+    else
+        popd -n > /dev/null
+    fi
 }
