37d3a9fffa1346d761a99313e1dd2e19fec43934
Paul Harris <paul.harris@eegeo.com>
2016-08-23 09:50:40 +0100

    Add OS basics for OSX

--- os-basics.sh
+++ os-basics.sh
@@ -1,5 +1,7 @@
 if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" ]; then
     source ~/bin/terminal-helpers/os-basics-windows.sh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	source ~/bin/terminal-helpers/os-basics-osx.sh
 else
     echo "[terminal-helpers] Error: Could not load basic OS helpers for OS of type $OSTYPE"
 fi
