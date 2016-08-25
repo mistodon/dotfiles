ab3363c61abfbee0fe48a3a3251c8d5a27ba7270
Vi <1149443+mistodon@users.noreply.github.com>
2016-08-25 17:17:19 +0100

    Give gstash pull, show, apply sub-commands

--- git-aliases.sh
+++ git-aliases.sh
@@ -9,9 +9,18 @@ alias gpom="git pull origin master"
 alias gpush="git push origin"
 alias gsub="git submodule foreach \"(git checkout master; git pull origin master)\""
 
-function gstashpull()
+function gstash()
 {
-    git stash
-    git pull origin $1
-    git stash pop
+    if [ "$1" == "pull" ]; then
+        git stash
+        git pull origin $2
+        git stash pop
+    elif [ "$1" == "show" ]; then
+        git stash show stash^{/"$2"}
+    elif [ "$1" == "apply" ]; then
+        git stash apply stash^{/"$2"}
+    else
+        echo "gstash: '$1' is not a valid command. (pull|show|apply)" 1>&2
+        return 1
+    fi
 }
