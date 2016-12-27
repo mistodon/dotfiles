e6b2ba6ff5d5916e5d58598611ebcc3078ba6039
Vi <1149443+mistodon@users.noreply.github.com>
2016-12-27 19:28:07 +0000

    Add Linux specific basics and rename osx to macos

--- os-basics.sh
+++ os-basics.sh
@@ -1,7 +1,9 @@
 if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" ]; then
     source ~/bin/terminal-helpers/os-basics-windows.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	source ~/bin/terminal-helpers/os-basics-osx.sh
+	source ~/bin/terminal-helpers/os-basics-macos.sh
+elif [[ "$OSTYPE" == "linux"* ]]; then
+	source ~/bin/terminal-helpers/os-basics-linux.sh
 else
     echo "[terminal-helpers] Error: Could not load basic OS helpers for OS of type $OSTYPE"
 fi
