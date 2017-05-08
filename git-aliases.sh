2c7ee8653da1002231c5c71b174118f0746155b7
Paul Harris <paul.harris@eegeo.com>
2017-05-08 12:11:50 +0100

    Fix gpush/gpull

--- git-aliases.sh
+++ git-aliases.sh
@@ -6,10 +6,28 @@ alias gc="git checkout"
 alias gb="git branch"
 alias gf="git fetch"
 alias gpom="git pull origin master"
-alias gpull="git pull origin HEAD"
-alias gpush="git push origin HEAD"
-alias gsub="git submodule foreach \"(git checkout master; git pull origin master)\""
 alias gcurr="git rev-parse --abbrev-ref HEAD"
+alias gsub="git submodule foreach \"(git checkout master; git pull origin master)\""
+
+
+function gpull()
+{
+    local branch="$(git rev-parse --abbrev-ref HEAD)"
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    git pull origin "$branch"
+}
+
+function gpush()
+{
+    local branch="$(git rev-parse --abbrev-ref HEAD)"
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    git push origin "$branch"
+}
+
 
 function gstash()
 {
