5b4378e0ee8487272dc8d6c0e104a89117818246
Vi <1149443+mistodon@users.noreply.github.com>
2017-06-23 13:32:01 +0100

    Add msvcup command for setting up C++ compiler on Windows

---
+++ windows-compiler-cli.sh
@@ -0,0 +1,6 @@
+function msvcup
+{
+    export PATH="C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin:C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Bin:C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE:$PATH"
+    export INCLUDE="C:\Program Files\Microsoft SDKs\Windows\v7.1\Include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10586.0\ucrt\;$INCLUDE"
+    export LIB="C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10586.0\ucrt\x86;$LIB"
+}
