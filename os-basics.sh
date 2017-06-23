944bddcd8e64e8805a6e188ffedc5dac5755b622
Paul Harris <paul.harris@eegeo.com>
2017-06-23 13:32:01 +0100

    Add msvcup command for setting up C++ compiler on Windows

--- os-basics.sh
+++ os-basics.sh
@@ -1,5 +1,6 @@
 if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" ]; then
     source ~/bin/terminal-helpers/os-basics-windows.sh
+    source ~/bin/terminal-helpers/windows-compiler-cli.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	source ~/bin/terminal-helpers/os-basics-macos.sh
 elif [[ "$OSTYPE" == "linux"* ]]; then
