295aa6054c94fbc4760d499ae98574bf5e83fdff
Vi <1149443+mistodon@users.noreply.github.com>
2017-07-07 11:30:57 +0100

    Split vimrc between unix and windows, add --login to shell

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,5 +2,12 @@ function vimsetup()
 {
     mkdir -p ~/.vim/colors
     cp ~/bin/terminal-helpers/vim/colors/* ~/.vim/colors
-    cp ~/bin/terminal-helpers/vim/.vimrc ~/.vimrc
+    cp ~/bin/terminal-helpers/vim/vimrc ~/.vimrc
+
+    if [ "$OSTYPE" == "msys" -o "$OSTYPE" == "cygwin" ]; then
+        echo "Warning: Ensure you have the unixify_paths script on your path (sed 's:\\\\:/:g')"
+        cat ~/bin/terminal-helpers/vim/vimrc_win_ext >> ~/.vimrc
+    else
+        cat ~/bin/terminal-helpers/vim/vimrc_unix_ext >> ~/.vimrc
+    fi
 }
