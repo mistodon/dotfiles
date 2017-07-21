3e88068f5e7d18d84e246669aa025305b952e681
Vi <1149443+mistodon@users.noreply.github.com>
2017-07-21 10:50:06 +0100

    Add msbuild function on Windows

--- windows-compiler-cli.sh
+++ windows-compiler-cli.sh
@@ -4,3 +4,8 @@ function msvcup
     export INCLUDE="C:\Program Files\Microsoft SDKs\Windows\v7.1\Include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Include;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\atlmfc\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.10586.0\ucrt\;$INCLUDE"
     export LIB="C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\lib;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10586.0\ucrt\x86;$LIB"
 }
+
+function msbuild
+{
+    cmd "/C %HOME%/bin/terminal-helpers/windows_bats/msbuild.bat $@"
+}
