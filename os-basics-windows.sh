206863441789106fc0d1cea1f5f96d17ff6061ad
Vi <1149443+mistodon@users.noreply.github.com>
2017-07-31 02:37:41 +0100

    Refactor everything into fewer files

--- os-basics-windows.sh
+++
@@ -1,8 +0,0 @@
-alias fm="explorer ."
-
-which make &>/dev/null
-if [ $? -ne 0 ]; then
-    alias make="mingw32-make"
-else
-    alias make="make"
-fi
--- web.sh
+++
@@ -1,5 +0,0 @@
-if [ `python --version 2>&1 | cut -c 8` == "2" ]; then
-    alias lhost="python -m SimpleHTTPServer 8000"
-else
-    alias lhost="python -m http.server"
-fi
