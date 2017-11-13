2750d6f60b4a046d48f8f1dc98ba964ebe255f16
Vi <1149443+mistodon@users.noreply.github.com>
2017-11-13 12:28:45 +0000

    Add pre-commit hook to prevent HEYJUDEs

--- git/configure_git.sh
+++ git/configure_git.sh
@@ -4,6 +4,7 @@ set -e
 
 readonly git_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 
+git config --global core.hooksPath ${git_dir}/hooks
 git config --global core.excludesfile ${git_dir}/gitignore
 git config --global core.editor vim
 git config --global user.useConfigOnly true
