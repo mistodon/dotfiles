c22e8244d26fa0b67ca0eb49185affcc8226be65
Vi <1149443+mistodon@users.noreply.github.com>
2018-01-15 15:48:26 +0000

    Fix a few mistakes in portable vimrc

--- bin/generate_portable_vimrc.py
+++ bin/generate_portable_vimrc.py
@@ -9,8 +9,11 @@ if __name__ == "__main__":
     runtimepath_pattern = re.compile(":?set runtimepath.*")
     vundle_pattern = re.compile(".*(V|v)undle.*")
     plugin_pattern = re.compile("\s*Plugin.*")
+    colorscheme_au_pattern = re.compile(".+colo(rscheme)?.*")
+    termgui_pattern = re.compile(".*termguicolors.*")
+    t_stuff_pattern = re.compile(".*t_8(f|b).*")
 
-    patterns = [runtimepath_pattern, vundle_pattern, plugin_pattern]
+    patterns = [runtimepath_pattern, vundle_pattern, plugin_pattern, colorscheme_au_pattern, termgui_pattern, t_stuff_pattern]
     for pattern in patterns:
         vimrc_content = pattern.sub("", vimrc_content)
 
